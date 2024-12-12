db5ff60e93f7a9b12f1d21e3f5046afa9982e20a
Fezao187 <katlegomaredi9@gmail.com>
2024-12-12 04:00:25 +0200

    Strings in python

---
+++ python_tut/strings.py
@@ -0,0 +1,7 @@
+# String methods/functions
+course = "Python for Beginners"
+print(course.upper()) #Convert to uppercase
+print(course.find('y')) #find first index of y
+print(course.find('for')) #find index of word
+print(course.replace('Python', 'Java')) #Replace words
+print("Python" in course)
