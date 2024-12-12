d3020d91cbd9c0d86717ad86aa59bfae6fe298f3
Fezao187 <katlegomaredi9@gmail.com>
2024-12-12 03:08:30 +0200

    Type conversions

---
+++ python_tut/type_conversion.py
@@ -0,0 +1,17 @@
+# Type conversion
+# Convert String to int
+birth_year=input("Enter birth year: ")
+age=2024- int(birth_year)
+print(age)
+
+# Covert values
+int() # to integer
+float() # to float
+bool() # to boolean
+str() # to String
+
+# Exercise sum of two numbers
+first_num=float(input("Enter first number: "))
+second_num=float(input("Enter second number: "))
+
+print("Sum: "+str(first_num+second_num))
