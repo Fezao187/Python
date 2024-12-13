45e0e6a3011dff914546b26cded3ffd4760c58e4
Fezao187 <katlegomaredi9@gmail.com>
2024-12-14 01:39:19 +0200

    List methods

--- python_tut/lists.py
+++ python_tut/lists.py
@@ -8,3 +8,15 @@ print(names)
 print(names[0:2])
 
 ### List methods
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+numbers.append(6)
+print(numbers)
+numbers.insert(0, 0)
+print(numbers)
+numbers.remove(3)
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
+numbers.clear()
+print(numbers)
