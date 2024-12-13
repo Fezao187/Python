d27defd9f0940ca4b674cebe377f98798f9db10c
Fezao187 <katlegomaredi9@gmail.com>
2024-12-13 15:44:27 +0200

    Operator precedence

--- python_tut/operatos.py
+++ python_tut/operatos.py
@@ -1,4 +1,4 @@
-# Arithmitic operations
+# Arithmetic operations
 print(10+3)
 print(10-3)
 print(10*3)
@@ -6,3 +6,11 @@ print(10/3) # =float
 print(10//3) # =int
 print(10%3)
 print(10**3) #power
+x = 10
+x=+3
+print(x)
+
+### Operator Precedence
+x = 10 + 3 * 2
+x =(10+3)*2
+print(x)
