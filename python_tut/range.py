37bea3cb13746383698326885ceb7613e84b1203
Fezao187 <katlegomaredi9@gmail.com>
2024-12-14 01:48:02 +0200

    Range function

---
+++ python_tut/range.py
@@ -0,0 +1,11 @@
+numbers = range(5)
+print(numbers)
+for number in numbers:
+    print(number)
+
+num = range(5, 10)
+for number in num:
+    print(number)
+
+for number in range(0, 10, 2): # Step 2
+    print(number)
