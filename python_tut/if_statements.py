30f464d38ec8919d73dfa34a8b668b1d1e6c6ede
Fezao187 <katlegomaredi9@gmail.com>
2024-12-14 00:54:01 +0200

    If statements

---
+++ python_tut/if_statements.py
@@ -0,0 +1,13 @@
+# If statements
+temparature = 25
+
+if temparature > 30:
+    print("It's very hot")
+    print("Drink plenty of water!")
+elif temparature > 20:
+    print("It's a nice day")
+elif temparature > 10:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+print("Done")
