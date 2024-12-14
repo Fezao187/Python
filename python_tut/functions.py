efbe4500cb8d6b954bb55f5c689b4a960e7fb04f
Fezao187 <katlegomaredi9@gmail.com>
2024-12-14 03:10:09 +0200

    Functions in Python

--- python_tut/functions.py
+++ python_tut/functions.py
@@ -31,3 +31,53 @@ print(message)
 file=open("name.txt", "w")
 file.write(message)
 file.close()
+
+def increment(number, by):
+    return number + by
+
+
+print(increment(number=2, by=3))
+
+### Default args
+def increment_1(number, by=1):
+    return number + by
+
+
+print(increment_1(number=2))
+
+# ANorther example (*args)
+# def multiply(*numbers):
+#     print(numbers)
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+print(multiply(1,2,3,4,5))
+
+### ***args
+def save_user(**user):
+    print(user['id'])
+    print(user['name'])
+
+
+save_user(id=1,name="Kristine", last_name="Wildheart")
+
+### Exercise(fizz, buzz)
+# Function returns different results depending on input
+# If input %3==0 return fizz
+# If input %5==0 return Buzz
+# If input %3 ==0 and %5==0 fizzbuzz
+# else return input
+
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    return number
+
+print(fizz_buzz(7))
