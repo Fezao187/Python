b0d1cc473cc7a92831260c0c06bfcc86ae6ba59b
Fezao187 <katlegomaredi9@gmail.com>
2024-12-31 00:31:16 +0200

    Custom view

--- django_rest_framework_tut/mysite/api/views.py
+++ django_rest_framework_tut/mysite/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
 
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
@@ -17,3 +18,18 @@ class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class=BlogPostSerializer
     lookup_field="pk"
+
+class BlogPostList(APIView):
+    def get(self, request,format=None):
+        # Get the title from the query parameters (if name, default to string)
+        title = request.query_params.get("title","")
+
+        if title:
+            # Filter the queryset based on the title
+            blog_posts=BlogPost.objects.filter(title_icontains=title)
+        else:
+            # If no title is provided, return all blogposts
+            blog_posts=BlogPost.objects.all()
+
+        serializer=BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
